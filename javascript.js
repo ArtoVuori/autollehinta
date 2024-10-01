c018856db108734b5006f7c1d2d11e093f54e69b
Arto Vuori <32056967+ArtoVuori@users.noreply.github.com>
2024-10-01 10:26:06 +0300

    More background images

--- javascript.js
+++ javascript.js
@@ -294,7 +294,7 @@ function clearSavedResults() {
 }
 
 function setRandomBackground() {
-    const maxImages = 3; // Update this number as you add more background images
+    const maxImages = 7; // Update this number as you add more background images
     const randomNumber = Math.floor(Math.random() * maxImages) + 1;
     const imageName = `back_${String(randomNumber).padStart(2, '0')}.jpg`;
     document.body.style.backgroundImage = `url('${imageName}')`;
